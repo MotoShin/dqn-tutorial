563bd430c2f654ef7ccf224f4532c5afff718501
Moto Shinriki <mshinrik@yahoo-corp.jp>
2020-11-20 12:03:41 +0900

    add schedule

--- cartpole/agent/policy/egreedy.py
+++ cartpole/agent/policy/egreedy.py
@@ -3,26 +3,23 @@ import random
 import math
 
 import utility
+from utility.schedule import LinearSchedule
 from agent.policy.greedy import Greedy
 from agent.policy.policymodel import PolicyModel
 
 
 class Egreedy(PolicyModel):
     def __init__(self, n_action):
-        self.epsilon_threshold = utility.EPS_START
-        self.epsilon_end = utility.EPS_END
-        self.epsilon_decay = utility.EPS_DECAY
         self.steps_done = 0
         self.n_action = n_action
+        self.exploration_schedule = LinearSchedule(utility.EPS_TIMESTEPS, utility.EPS_END, utility.EPS_START)
 
     def select(self, set: torch.Tensor):
         sample = random.random()
-        self.steps_done += 1
         selected = None
-        if sample > self.epsilon_threshold:
+        if sample > self.exploration_schedule.value(self.steps_done):
             selected =  Greedy().select(set)
         else:
             selected = torch.tensor([[random.randrange(self.n_action)]], device=utility.device, dtype=torch.long)
-        self.epsilon_threshold *= self.epsilon_decay
-        self.epsilon_threshold = max(self.epsilon_end, self.epsilon_threshold)
+        self.steps_done += 1
         return selected
