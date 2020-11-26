74d9b791d65bb3fad54a88229e62f39293b85e4d
Moto Shinriki <m8o8t2o8@gmail.com>
2020-11-26 22:08:04 +0900

    fix network input in simulation

--- cartpole/simulation/simulate.py
+++ cartpole/simulation/simulate.py
@@ -7,6 +7,7 @@ import utility
 from agent.agentmodel import Agent
 from agent.learningmethod.dqn.dqn import DqnLearningMethod
 from agent.learningmethod.dqn.dqnsoftupdate import DqnSoftUpdateLearningMethod
+from agent.learningmethod.model import Variable
 from agent.policy.greedy import Greedy
 from agent.policy.egreedy import Egreedy
 from environment.cartpole import CartPole
@@ -71,7 +72,8 @@ class Simulate(object):
             last_idx = self.agent.save_memory(state)
 
             # Chose action
-            inp = torch.from_numpy(np.array([self.agent.get_screen_history()])).type(torch.FloatTensor).type(utility.dtype) / 255.0
+            recent_screen = self.agent.get_screen_history()
+            inp = Variable(torch.from_numpy(np.array([recent_screen])).type(utility.dtype) / 255.0)
             action = self.agent.select_action(inp, episode_num)
             # Action
             _, reward, done, _ = self.env.step(action)
